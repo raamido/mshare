f0be6b2dc34a588b9c43a2d90af7de70b9c7aaaf
Rami <ramiabrahem24@gmail.com>
2023-03-17 20:43:50 +0200

    update url when generate secret message

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// initial commit
+const form = document.querySelector('form')
+const messageBox = document.querySelector('textarea')
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const SECRET_MESSAGE = btoa(messageBox.value)
+    const params = new URLSearchParams(location.search)
+    params.set("message", SECRET_MESSAGE)
+    messageURL = location.href + '?' + params.toString();
+
+    history.pushState(null, '', messageURL)
+    event.target.reset()
+})
