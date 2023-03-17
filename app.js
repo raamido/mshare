d7f322f465bdbe2353c9b134464c10cad6c58541
Rami <ramiabrahem24@gmail.com>
2023-03-17 21:28:23 +0200

    update ui when creating a message

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const form = document.querySelector('form')
 const messageBox = document.querySelector('textarea')
+const messageContainer = document.querySelector('.message')
 
+window.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(location.search)
+    const message = params.get("message")
+
+    if (message) {
+        form.hidden = true
+        messageContainer.hidden = false
+
+        messageContainer.querySelector('p').textContent = atob(message)
+    } else {
+        form.hidden = false
+        messageContainer.hidden = true
+    }
+})
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -12,4 +27,15 @@ form.addEventListener('submit', (event) => {
 
     history.pushState(null, '', messageURL)
     event.target.reset()
+    form.hidden = true
+    messageContainer.hidden = false
+
+    messageContainer.querySelector('p').textContent = atob(SECRET_MESSAGE)
+})
+
+window.addEventListener('popstate', (event) => {
+    if (!event.state) {
+        form.hidden = false
+        messageContainer.hidden = true
+    }
 })
